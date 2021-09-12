c216607666d27d315f9fc8d385c1ca338e05739f
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-09-13 01:31:54 +0530

    predict maximum a posteriori with log likelihood

--- naive_bayes_impl.py
+++ naive_bayes_impl.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import multivariate_normal as mvn
 
 # Implementing Gaussian Naive Bayes
 class NaiveBayes(object):
@@ -15,3 +16,11 @@ class NaiveBayes(object):
             }
             self.priors[c] = float(len(Y[Y == c])) / len(Y)
             
+    def predict(self, X):
+        N, D = X.shape
+        K = len(self.gaussians)
+        P = np.zeros((N, K))
+        for c, g in iteritems(self.gaussians):
+            mean, var = g['mean'], g['var']
+            P[:,c] = mvn.logpdf(X, mean=mean, cov=var) + np.log(self.priors[c])
+        return np.argmax(P, axis=1)
